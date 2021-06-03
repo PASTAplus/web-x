0a8133f35973e37b0fdf038ca3c43d5230e528d3
Mark Servilla <mark.servilla@gmail.com>
2021-06-03 10:07:51 -0600

    Reconfigure cli vs server start in main

--- main.py
+++ main.py
@@ -36,12 +36,8 @@ def configure_routes():
     app.include_router(home.router)
 
 
-def main():
+if __name__ == "__main__":
     configure(dev_mode=True)
     uvicorn.run(app, host='127.0.0.1', port=8000, debug=True)
-
-
-if __name__ == "__main__":
-    main()
 else:
     configure(dev_mode=False)
