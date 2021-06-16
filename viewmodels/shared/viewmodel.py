4f643fb15d4f98836a3535dde35c2c5c94b47b63
Mark Servilla <mark.servilla@gmail.com>
2021-06-15 21:06:03 -0600

    Add session test for logged in user

--- viewmodels/shared/viewmodel.py
+++ viewmodels/shared/viewmodel.py
@@ -16,16 +16,17 @@ from typing import Optional
 
 from starlette.requests import Request
 
+from services import cookie_auth
+
 
 class ViewModelBase:
 
     def __init__(self, request: Request):
         self.request: Request = request
         self.error: Optional[str] = None
-        self.user_id: Optional[int] = None
+        self.user_id: Optional[str] = None
 
-        # We'll get this once we have users from the cookies.
-        self.is_logged_in = False
+        self.is_logged_in, self.common_name = cookie_auth.get_user_via_auth_cookie(self.request)
 
     def to_dict(self) -> dict:
         return self.__dict__
