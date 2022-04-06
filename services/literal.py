3a76aa3c6dd661b88fd6dde0c6ee95fa3b965025
Mark Servilla <mark.servilla@gmail.com>
2022-04-05 19:31:31 -0600

    Add Literal class to keep chameleon from escaping pure HTML

---
+++ services/literal.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+:Mod: literal
+
+:Synopsis:
+
+:Author:
+    servilla
+
+:Created:
+    4/5/22
+"""
+
+
+class Literal(object):
+    def __init__(self, s):
+        self.s =s
+
+    def __html__(self):
+        return self.s
