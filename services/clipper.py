fbb2976d721250a43cab25a686ae2e3cfd73d543
Mark Servilla <mark.servilla@gmail.com>
2022-04-07 09:49:47 -0600

    Add sentence clipping utility

---
+++ services/clipper.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+:Mod: clipper
+
+:Synopsis:
+
+:Author:
+    servilla
+
+:Created:
+    4/7/22
+"""
+
+
+def clip(sentence: str, length: int) -> str:
+    clipped = ""
+    words = sentence.split()
+    for word in words:
+        if len(clipped) + len(word) < length - 3:
+            clipped = " ".join([clipped, word])
+        else:
+            clipped += "..."
+            break
+    return clipped.strip()
