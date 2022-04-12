15a32ea10147d164af46a0b7efcf10828b89f3ac
Mark Servilla <mark.servilla@gmail.com>
2022-04-12 11:42:34 -0600

    Support display of the latest featured and news posts

--- viewmodels/index_viewmodel.py
+++ viewmodels/index_viewmodel.py
@@ -16,10 +16,13 @@ from typing import List
 
 from starlette.requests import Request
 
+from services import post_service
 from viewmodels.shared.viewmodel import ViewModelBase
 
 
 class IndexViewModel(ViewModelBase):
-    def __init__(self, request: Request):
-        super().__init__(request)
+    def __init__(self, request: Request, title: str = "Environmental Data Initiative"):
+        super().__init__(request, title)
 
+        self.featured = post_service.get_postcard_objects(post_type="featured", clip_len=200, img_picker="pickme")
+        self.news = post_service.get_postcard_objects(post_type="news", clip_len=200, img_picker="pickme")
