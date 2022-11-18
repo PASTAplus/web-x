0f27b2cb64475a56aec7f5924134de6ac3fda749
Mark Servilla <mark.servilla@gmail.com>
2022-11-17 19:17:25 -0700

    Redefine function name in accordance with route

--- views/about.py
+++ views/about.py
@@ -23,34 +23,41 @@ router = fastapi.APIRouter()
 
 @router.get('/about/about-edi')
 @template("about/about-edi.html")
-def edi(request: Request):
+def about_edi(request: Request):
     vm = ViewModelBase(request, "About EDI")
-    return vm.to_dict()
-
-
-@router.get('/about/edi-policy')
-@template("about/edi-policy.html")
-def policies(request: Request):
-    vm = ViewModelBase(request, "About Policies")
-    return vm.to_dict()
-
-
-@router.get('/about/partners-and-affiliations')
-@template("about/partners-and-affiliations.html")
-def partners(request: Request):
-    vm = ViewModelBase(request, "About Partners and Affiliations")
-    return vm.to_dict()
-
-
-@router.get('/about/edi-products')
-@template("about/edi-products.html")
-def partners(request: Request):
-    vm = ViewModelBase(request, "EDI Products")
     return vm.to_dict()
 
 
 @router.get('/about/edi-advisory-board')
 @template("about/edi-advisory-board.html")
-def advisors(request: Request):
+def edi_advisory_board(request: Request):
     vm = ViewModelBase(request, "EDI Advisory Board")
+    return vm.to_dict()
+
+
+@router.get('/about/edi-policy')
+@template("about/edi-policy.html")
+def edi_policy(request: Request):
+    vm = ViewModelBase(request, "About Policies")
+    return vm.to_dict()
+
+
+@router.get('/about/edi-products')
+@template("about/edi-products.html")
+def edi_products(request: Request):
+    vm = ViewModelBase(request, "EDI Products")
+    return vm.to_dict()
+
+
+@router.get('/about/edi-public-calendar')
+@template("about/edi-public-calendar.html")
+def edi_public_calendar(request: Request):
+    vm = ViewModelBase(request, "EDI Public Calendar")
+    return vm.to_dict()
+
+
+@router.get('/about/partners-and-affiliations')
+@template("about/partners-and-affiliations.html")
+def partners_and_affiliations(request: Request):
+    vm = ViewModelBase(request, "About Partners and Affiliations")
     return vm.to_dict()
